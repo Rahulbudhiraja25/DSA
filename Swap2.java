c3695c772dadd969df2f7c3c784fd3831561ac69
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-03-29 11:24:12 +0530

    Completed problem Write a program to swap two numbers without using a third variable

---
+++ Swap2.java
@@ -0,0 +1,24 @@
+// Write a program to swap two numbers without using a third variable
+import java.util.*;
+public class Swap2{
+    public static void main(String[] args) {
+        int a =5;
+        int b=6;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+        //   a=a+b;
+        //   b=a-b;
+        //   a=a-b;
+      
+        a=a*b;
+        b=a/b;
+        a=a/b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
