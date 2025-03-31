32f042d00a19195c60d8f2784fb7ac1b4f9ef0cc
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-03-31 15:23:04 +0530

    Write program to find HCF two numbers

---
+++ HCF.java
@@ -0,0 +1,17 @@
+//Program to find HCF two nubers
+public class HCF {
+
+    public static void main(String[] args) {
+        int a=12,b=18;
+
+        int result=Math.min(a,b);
+       while(result>0){
+           if(a%result==0&&b%result==0){
+               break;
+           }
+           result--;
+       }
+System.out.println("Gst of a and b is "+result);
+    }
+
+}
